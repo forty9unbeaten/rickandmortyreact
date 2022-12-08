970fb9cb3c05a906a369db9b6b5076e89c012a54
Rob Spears <rob.sp3ars@gmail.com>
2022-12-08 12:15:39 -0500

    update favicon and app title

--- apps/rick-and-morty/src/app/app.tsx
+++ apps/rick-and-morty/src/app/app.tsx
@@ -1,14 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
-import NxWelcome from './nx-welcome';
 
 export function App() {
-  return (
-    <>
-      <NxWelcome title="rick-and-morty" />
-      <div />
-    </>
-  );
+  return <div></div>;
 }
 
 export default App;
